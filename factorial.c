c553e1ae1a07c6bb8129b441594f018c968fef15
Somil <somilvijay31@gmail.com>
2022-12-11 21:58:56 +0530

    factorial of number in put by user

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+
+    
+    
+    
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
+
+    }
+    printf("the value of factorial is:%d",fact);
+    return 0;
+
+
+    
+}
